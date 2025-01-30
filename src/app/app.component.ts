ef44faaccdaf923ca5457b9aff463a28e29e2453
Toitoine520 <alenagard57@gmail.com>
2025-01-30 10:42:13 +0100

    Cours intermediaire 1 : Les observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { interval, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  interval$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000);
+
+  }
+}
