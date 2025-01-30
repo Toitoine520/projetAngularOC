7346c661e01d9c8934122103e221af34327b71ac
Toitoine520 <alenagard57@gmail.com>
2025-01-30 16:32:19 +0100

    Parcours intermediaire 2 et 3 : observables et operateurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { interval, Observable } from 'rxjs';
+import { combineLatest, filter, interval, map, Observable, Subscription, tap, timer } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { OperatorSubscriber } from 'rxjs/internal/operators/OperatorSubscriber';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,39 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  interval$!: Observable<string>;
 
-  interval$!: Observable<number>;
+  timer1$: Observable<number> = timer(1000, 4000);
+  timer2$: Observable<number> = timer(2000, 4000);
+  timer3$: Observable<number> = timer(3000, 4000);
+
+  timers$!: Subscription;
 
   ngOnInit(): void {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      filter((value) => value % 3 !== 0),
+      map((value) =>
+        value < 10
+          ? `${value} est inférieur 10`
+          : value === 10
+          ? `${value} est égal à 10`
+          : `${value} est supérieur à 10`
+      ),
+      tap((value) => this.logger(value))
+    );
 
+    this.timers$ = combineLatest(this.timer1$, this.timer2$, this.timer3$).subscribe(
+      ([timer1, timer2, timer3]) => {
+        console.log(
+          `timer 1 : ${timer1}`,
+          `timer 2 : ${timer2}`,
+          `timer 3 : ${timer3}`,
+        );
+      }
+    );
+  }
+
+  logger(texte: string) {
+    console.log(`Log : ${texte}`);
   }
 }
